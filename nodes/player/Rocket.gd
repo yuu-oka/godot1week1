99100fabc8520253e96c706c51c7142eb1396005
yuu-oka <okada.yusaku@gmail.com>
2023-10-31 03:21:50 +0900

    add player - rocket

---
+++ nodes/player/Rocket.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+@export var speed = 400
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO
+	
+	if Input.is_action_just_pressed("rocket_move_right"):
+		velocity.x += 1
+	if Input.is_action_just_pressed("rocket_move_left"):
+		velocity.x -= 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	pass
