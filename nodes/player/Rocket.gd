2eaaa4991e9126edc410e0ca9b352bea87745af0
yuu-oka <okada.yusaku@gmail.com>
2023-10-31 05:16:08 +0900

    移動の修正、当たり判定処理の追加

--- nodes/player/Rocket.gd
+++ nodes/player/Rocket.gd
@@ -3,6 +3,14 @@ extends Area2D
 @export var speed = 400
 var screen_size
 
+signal hit
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+	pass
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -13,9 +21,9 @@ func _ready():
 func _process(delta):
 	var velocity = Vector2.ZERO
 	
-	if Input.is_action_just_pressed("rocket_move_right"):
+	if Input.is_key_pressed(KEY_RIGHT) && Input.is_key_pressed(KEY_Z):
 		velocity.x += 1
-	if Input.is_action_just_pressed("rocket_move_left"):
+	if Input.is_key_pressed(KEY_LEFT) && Input.is_key_pressed(KEY_Z):
 		velocity.x -= 1
 	
 	if velocity.length() > 0:
@@ -24,3 +32,9 @@ func _process(delta):
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
 	pass
+
+func _on_body_entered(body):
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
+	pass # Replace with function body.
